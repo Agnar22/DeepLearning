Fix softmax backprop

Main.py
@@ -41,8 +41,8 @@ def train_network(model, x_train, y_train, x_val, y_val):
 
 
 if __name__ == '__main__':
-    print(read_config())
     data = np.genfromtxt("Data/train_small.csv", delimiter=',')
+    val_data = np.genfromtxt("Data/validate_small.csv", delimiter=',')
 
     # import cv2
     #
@@ -52,12 +52,16 @@ if __name__ == '__main__':
     #     cv2.destroyAllWindows()
     inp = Layers.Input(784)
     b = Layers.Dense(512, activation=Activations.ReLu(), use_bias=True)(inp)
-    b = Layers.Dense(512, activation=Activations.ReLu(), use_bias=True)(b)
-    b = Layers.Dense(512, activation=Activations.ReLu(), use_bias=True)(b)
+    # b = Layers.Dense(512, activation=Activations.Linear(), use_bias=True)(b)
+    b = Layers.Dense(512, activation=Activations.Tanh(), use_bias=True)(b)
     b = Layers.Dense(10, activation=Activations.Softmax(), use_bias=True)(b)
+    # b = Activations.Softmax()(b)
     model = NN.Models.Sequential()
     model.add(b)
 
-    model.compile(loss=NN.Losses.L2(), lr=0.0000001)
+    model.compile(loss=NN.Losses.Cross_Entropy(), lr=0.0000001)
     model.fit(data[:, :784],
-              np.array([[1 if x == data[y, -1] else 0 for x in range(10)] for y in range(data.shape[0])]), epochs=200)
+              np.array([[1 if x == data[y, -1] else 0 for x in range(10)] for y in range(data.shape[0])]),
+              validation_data=(val_data[:, :784],
+              np.array([[1 if x == val_data[y, -1] else 0 for x in range(10)] for y in range(data.shape[0])]))
+    , epochs=200)

NeuralNetwork/Activations.py
@@ -9,17 +9,32 @@ class Activation:
         self.prev_layer = None
 
     def __call__(self, input):
+        """
+
+        :param input:
+        :return:
+        """
         self.prev_layer = input
         self.output_shape = self.prev_layer.output_shape
         return self
 
     def forward(self, input):
+        """
+
+        :param input:
+        :return:
+        """
         if self.prev_layer is not None:
             input = self.prev_layer.forward(input)
         self.activations = self.function(input)
         return self.activations
 
     def backward(self, temp_gradient):
+        """
+
+        :param temp_gradient:
+        :return:
+        """
         gradient = self.derivative(self.activations) * temp_gradient
         if self.prev_layer is not None:
             self.prev_layer.backward(gradient)
@@ -51,8 +66,28 @@ class Softmax(Activation):
         Activation.__init__(self,
                             lambda x: np.exp(x - np.max(x, axis=-1, keepdims=True)) /
                                       np.exp(x - np.max(x, axis=-1, keepdims=True)).sum(axis=-1, keepdims=True),
-                            lambda x: -np.power(np.e, x) * (1 - np.power(np.e, x))
-                            )
+                            lambda x: x)
+
+    def backward(self, temp_gradient):
+        """
+
+        :param temp_gradient:
+        :return:
+        """
+        # print((self.activations.transpose() @ self.activations))
+        # print((1 - np.identity(self.activations.shape[-1])))
+        # print(-(self.activations.transpose() @ self.activations) * (1 - np.identity(self.activations.shape[-1])))
+        act_shape = self.activations.shape
+        act = self.activations.reshape(act_shape[0], 1, act_shape[-1])
+        jacobian = - (act.transpose((0, 2, 1)) @ act) * (1 - np.identity(self.activations.shape[-1]))
+        jacobian += np.identity(act_shape[-1]) * (act * (1 - act)).transpose((0, 2, 1))
+
+        gradient = (jacobian @ temp_gradient.reshape(act_shape[0], act_shape[-1], 1))  #
+        gradient = gradient.reshape((act_shape[0], act_shape[-1]))
+        if self.prev_layer is not None:
+            self.prev_layer.backward(gradient)
+        else:
+            return gradient
 
 
 if __name__ == '__main__':

NeuralNetwork/Layers.py
@@ -69,23 +69,23 @@ class Dense:
         delta_bias = temp_gradient.transpose().sum(axis=-1, keepdims=True)
 
         # Update weights
-        self.weights -= 0.00001 * delta_weights
-        print("gradients", delta_weights.flatten().mean())
-        self.bias -= 0.00001 * delta_bias
+        self.weights -= 0.001 * delta_weights
+        # print("gradients", delta_weights.flatten().mean())
+        self.bias -= 0.001 * delta_bias
 
         self.prev_layer.backward(next_grad)
 
 
 if __name__ == '__main__':
     inp = Input(1)
-    b = Dense(40, activation=Activations.ReLu())(inp)
+    b = Dense(40, activation=Activations.ReLu(), use_bias=True)(inp)
     # b = Dense(50, activation=Activations.ReLu())(b)
     # b = Dense(60, activation=Activations.ReLu())(b)
     # b = Dense(100, activation=Activations.ReLu())(inp)
     # b = Dense(10, activation=Activations.ReLu())(b)
     # b = Dense(2, activation=Activations.Linear())(b)
-    b = Dense(2, activation=Activations.Softmax(), use_bias=True)(b)
-    # b = Activations.Linear()(b)
+    b = Dense(4, activation=Activations.Softmax(), use_bias=True)(b)
+    # b = Activations.ReLu()(b)
     # b = Activations.Linear()(b)
     # b = Activations.Linear()(b)
     # b = Activations.Linear()(b)
@@ -98,10 +98,10 @@ if __name__ == '__main__':
         print(outp, outp.shape)
         # loss = Losses.L2()
         loss = Losses.Cross_Entropy()
-        # print("loss", loss.forward(outp, np.array([[0.1, 0.1, 0.1, 0.4], [0.4, 0.2, 1, 2]])))
-        # print("loss", loss.backward(outp, np.array([[0.1, 0.1, 0.1, 0.4], [0.4, 0.2, 1, 2]])))
-        # b.backward(loss.backward(outp, np.array([[0.1, 0.1, 0.1, 0.4], [0.4, 0.2, 1, 2]])))
+        print("loss", loss.forward(outp, np.array([[0.4, 0.1, 0.1, 0.4], [0.4, 0.2, 0.2, 0.2]])))
+        print("loss", loss.backward(outp, np.array([[0.4, 0.1, 0.1, 0.4], [0.4, 0.2, 0.2, 0.2]])))
+        b.backward(loss.backward(outp, np.array([[0.4, 0.1, 0.1, 0.4], [0.4, 0.2, 0.2, 0.2]])))
         # b.backward(loss.backward(outp, np.array([[1], [0]])))
         # print("loss", loss.forward(outp, np.array([[1], [0]])))
-        b.backward(loss.backward(outp, np.array([[0.1, 0.9], [0.9, 0.1]])))
-        print("loss", loss.forward(outp, np.array([[0.1, 0.9], [0.9, 0.1]])))
+        # b.backward(loss.backward(outp, np.array([[0.1, 0.9], [0.9, 0.1]])))
+        # print("loss", loss.forward(outp, np.array([[0.1, 0.9], [0.9, 0.1]])))

NeuralNetwork/Losses.py
@@ -22,7 +22,7 @@ class L2(Loss):
 
 class Cross_Entropy(Loss):
     def __init__(self):
-        Loss.__init__(self, lambda x, y: -y * np.log2(x), lambda x, y: 1 + (-y / (x)))  # * np.log(2)))
+        Loss.__init__(self, lambda x, y: -y * np.log2(x+0.00001), lambda x, y: -y / (x+0.00001))  # * np.log(2)))
 
 
 if __name__ == '__main__':

NeuralNetwork/Models.py
@@ -1,4 +1,5 @@
 # import Layers
+import numpy as np
 
 
 class Sequential:
@@ -22,33 +23,36 @@ class Sequential:
         self.loss_func = loss
         self.lr = lr
 
-    def fit(self, x_train, y_train, validation_data=None, epochs=1, batch_size=32):
+    def fit(self, x_train, y_train, validation_data=None, epochs=10, batch_size=32):
         train_loss = []
         val_loss = []
-        print(y_train)
         for epoch in range(epochs):
+            correct = 0
             loss = 0
             for x in range(0, x_train.shape[0], batch_size):
                 increment = min(batch_size, x_train.shape[0] - x)
 
-                fwd_propagation = self.layers[-1].forward(x_train[x:x + increment].reshape(increment, x_train.shape[-1]))
+                fwd_propagation = self.layers[-1].forward(
+                    x_train[x:x + increment].reshape(increment, x_train.shape[-1]))
+                correct += (np.argmax(fwd_propagation, axis=-1) == np.argmax(y_train[x:x + increment], axis=-1)).sum()
                 temp_loss = self.loss_func.forward(fwd_propagation, y_train[x:x + increment])
                 loss += temp_loss * increment
 
-                loss_grad = self.loss_func.forward(fwd_propagation, y_train[x:x + increment])
+                loss_grad = self.loss_func.backward(fwd_propagation, y_train[x:x + increment])
                 self.layers[-1].backward(loss_grad)
 
-                Sequential.print_progress(40, x + increment, x_train.shape[0], loss)
+                Sequential.print_progress(40, x + increment, x_train.shape[0], loss, correct)
             train_loss.append(loss / x_train.shape[0])
 
             if validation_data is not None:
                 x_val, y_val = validation_data
 
                 fwd_propagation = self.layers[-1].forward(x_val)
-                val_loss = self.loss_func.forward(fwd_propagation, y_val)
+                correct_val = (np.argmax(fwd_propagation, axis=-1) == np.argmax(y_val, axis=-1)).sum()
+                curr_loss = self.loss_func.forward(fwd_propagation, y_val)
 
-                Sequential.print_progress(40, x_train.shape[0], loss, val_loss)
-            val_loss.append(loss)
+                Sequential.print_progress(40, x_train.shape[0], x_train.shape[0], loss, correct, loss_val=curr_loss, correct_val=correct_val, val_samp=y_val.shape[0])
+            val_loss.append(curr_loss)
             print()
         return train_loss, val_loss
 
@@ -56,17 +60,21 @@ class Sequential:
         pass
 
     @staticmethod
-    def print_progress(bars, batch_end, epoch_length, sum_loss, loss_val=None):
-
+    def print_progress(bars, batch_end, epoch_length, sum_loss, correct,
+                       loss_val=None, correct_val=None, val_samp=None):
         r = int((batch_end / epoch_length) * bars)
         progressbar = '\r[' + ''.join('=' for _ in range(r)) + '>' + ''.join('-' for _ in range(bars - r)) + '] '
         progress = str(round(batch_end * 100 / epoch_length, 3)) + ' % (' + \
                    str(batch_end) + '/' + str(epoch_length) + ')'
-        train_stats = '\tloss: ' + str(round(sum_loss / batch_end, 5))
+        train_stats = '\tloss: ' + str(round(sum_loss / batch_end, 5)) + \
+                      '\tcor: ' + str(correct) + '/' + str(batch_end) + \
+                      ' (' + str(round(correct * 100 / batch_end, 4)) + ' %)'
 
         val_stats = ''
         if loss_val is not None:
-            val_stats = '\tval_loss: ' + str(round(loss_val, 5))
+            val_stats = '\tval_loss: ' + str(round(loss_val, 5)) + \
+                        '\tval_cor: ' + str(correct_val) + '/' + str(val_samp) + \
+                        ' (' + str(round(correct_val * 100 / val_samp, 4)) + ' %)'
         print(progressbar + progress + train_stats + val_stats, end='')
 
 