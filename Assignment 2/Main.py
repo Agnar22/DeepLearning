e8270f088edd515c96b6f32c814ad39ca1c45a74
Agnar22 <agnar.bjornstad@gmail.com>
2020-02-26 15:58:53 +0100

    Define pivotal parameters and create keras model

--- Assignment 2/Main.py
+++ Assignment 2/Main.py
@@ -1,11 +1,42 @@
 import numpy as np
 import json
 from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import cv2
+import Model_tf
 
 
 def load_json(filepath: str) -> json:
     with open(filepath) as f:
         return json.load(f)
+
+
+def plot_graphs(close, *args):
+    # TODO: what to visualize?
+    # autoencoder training
+    # semi-supervised learning and supervised training
+    """
+    :param close:
+    :param args:
+    :return:
+    """
+
+    if close: plt.close('all')
+    for func in args:
+        plt.plot(func['x'], func['y'], label=func['name'])
+    plt.legend()
+    plt.show()
+
+def run_tsne(cases):
+    pass
+
+
+
+def display_images(img1, img2):
+    for num, (imgl, imgr) in enumerate(zip(img1, img2)):
+        vis = np.concatenate((imgl, imgr), axis=1)
+        cv2.imshow(str(num), vis)
+    cv2.waitKey()
 
 
 if __name__ == '__main__':
@@ -19,3 +50,6 @@ if __name__ == '__main__':
     # Modularity of autoencoder
     # Can have hidden layers after encoder
     print(load_json("config.json"))
+
+    data = Model_tf.x_train
+    display_images([data[x].reshape(28, 28, 1) for x in range(10)], [data[y].reshape(28, 28, 1) for y in range(20, 30)])
---
+++ Assignment 2/Model_tf.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Conv2D, Conv2DTranspose, Dense
+from tensorflow.keras import Model
+from tensorflow.keras.optimizers import SGD
+import cv2
+import numpy as np
+
+
+def create_autoencoder(input_shape):
+    input_data = Input(shape=input_shape)
+    # model = tf.keras.Sequential()
+    latent_vec = Dense(80, activation='linear')(input_data)
+    # latent_vec = Conv2D(1, (3, 3), activation='relu', kernel_regularizer='l2')(input_data)
+    # x = Conv2DTranspose(1, (3, 3), activation='sigmoid', kernel_regularizer='l2')(latent_vec)
+    # model.add(tf.keras.layers.Conv2D(32, (3, 3)))
+    x = Dense(784, activation='sigmoid')(latent_vec)
+
+    return Model(input_data, x, name='autoencoder'), Model(input_data, latent_vec, name='encoder')
+
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train.reshape(60000, 784) / 255
+# autoencoder, encoder = create_autoencoder((784))
+# autoencoder.compile(optimizer=SGD(learning_rate=0.01, momentum=0.9), loss='mse')
+
+# image = autoencoder.predict(x_train[0].reshape(1, 784))
+# image = np.array(image).reshape(28, 28, 1)
+# cv2.imshow("orig", np.array(x_train[0]).reshape(28, 28, 1))
+# image = np.array(image).reshape(28, 28, 1)
+# cv2.imshow("pred", image)
+
+# print(encoder.predict(x_train[0].reshape(1,784)).shape)
+
+# for x in range(4):
+#     autoencoder.fit(x_train, x_train, batch_size=8, epochs=1)
+#
+# for x in range(1000):
+#     image = autoencoder.predict(x_train[x].reshape(1, 784))
+#     image = np.array(image).reshape(28, 28, 1)
+#     cv2.imshow("orig", np.array(x_train[x]).reshape(28, 28, 1))
+#     cv2.imshow("pred", image)
+#     cv2.waitKey()
