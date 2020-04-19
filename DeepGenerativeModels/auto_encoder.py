b3daebaee59e302e33cda436823c817fbab5968a
Agnar22 <agnar.bjornstad@gmail.com>
2020-04-19 17:19:21 +0200

    Create variational autoencoder.

--- DeepGenerativeModels/auto_encoder.py
+++ DeepGenerativeModels/auto_encoder.py
@@ -1,15 +1,63 @@
-from keras.layers import Conv2D, Conv2DTranspose, Dense, Flatten, Input, MaxPooling2D, Reshape, SeparableConv2D
-from keras.layers import LeakyReLU, BatchNormalization
+from keras.layers import Conv2D, Conv2DTranspose, Dense, Flatten, Input, Reshape
+from keras.layers import LeakyReLU, BatchNormalization, Lambda
+from keras.losses import binary_crossentropy, mse
 from keras.models import Model
+from keras import backend as K
+import tensorflow as tf
 
 
-class AutoEncoder:
+def create_auto_encoder(input_shape, latent_size):
+    # Creating the encoder
+    input_layer_encoder = Input(shape=input_shape)
+    x = Conv2D(64, (3, 3), strides=2, use_bias=True, padding="same")(input_layer_encoder)
+    x = LeakyReLU(alpha=0.05)(x)
+    x = BatchNormalization(axis=-1)(x)
+    x = Conv2D(128, (3, 3), strides=2, activation='relu', use_bias=True, padding="same")(x)
+    x = LeakyReLU(alpha=0.05)(x)
+    x = BatchNormalization(axis=-1)(x)
+    x = Conv2D(256, (3, 3), strides=2, activation='relu', use_bias=True, padding="same")(x)
+    x = LeakyReLU(alpha=0.05)(x)
+    x = BatchNormalization(axis=-1)(x)
+    x = Flatten()(x)
+    x = Dense(512, use_bias=True)(x)
+    x = LeakyReLU(alpha=0.05)(x)
+    x = BatchNormalization(axis=-1)(x)
+    x = Dense(latent_size, activation='linear', use_bias=True)(x)
+    latent_vec = BatchNormalization(axis=-1)(x)
+    encoder = Model(input_layer_encoder, latent_vec)
 
+    # Creating the decoder
+    input_layer_decoder = Input(shape=encoder.layers[-1].output_shape)
+    x = Dense(512, use_bias=True)(input_layer_decoder)
+    x = LeakyReLU(alpha=0.05)(x)
+    x = BatchNormalization(axis=-1)(x)
+    x = Reshape((4, 4, 32))(x)
+    x = Conv2DTranspose(256, (4, 4), use_bias=True,
+                        padding='valid')(x)
+    x = LeakyReLU(alpha=0.05)(x)
+    x = BatchNormalization(axis=-1)(x)
+    x = Conv2DTranspose(128, (3, 3), strides=2, use_bias=True,
+                        padding='same')(x)
+    x = LeakyReLU(alpha=0.05)(x)
+    x = BatchNormalization(axis=-1)(x)
+    x = Conv2DTranspose(input_shape[-1], (3, 3), strides=2, activation='sigmoid',
+                        use_bias=True, padding='same')(x)
+    decoder = Model(input_layer_decoder, x, name='decoder')
+
+    auto_encoder = Model(input_layer_encoder, decoder(latent_vec), name='auto_encoder')
+
+    auto_encoder.summary()
+    for layer in auto_encoder.layers:
+        print(layer.output_shape)
+
+    return auto_encoder, encoder, decoder
+
+
+class VAE:
     def __init__(self, input_shape, latent_size):
-        (self.auto_encoder, self.encoder, self.decoder) = self.__create_auto_encoder(input_shape, latent_size)
+        self.vae, self.encoder, self.decoder = self.create_variational_auto_encoder(input_shape, latent_size)
 
-    def __create_auto_encoder(self, input_shape, latent_size):
-        # TODO: convolutions with strides > 1, no maxpooling?
+    def create_variational_auto_encoder(self, input_shape, latent_size):
         # Creating the encoder
         input_layer_encoder = Input(shape=input_shape)
         x = Conv2D(64, (3, 3), strides=2, use_bias=True, padding="same")(input_layer_encoder)
@@ -25,15 +73,13 @@ class AutoEncoder:
         x = Dense(512, use_bias=True)(x)
         x = LeakyReLU(alpha=0.05)(x)
         x = BatchNormalization(axis=-1)(x)
-        x = Dense(latent_size, activation='linear', use_bias=True)(x)
-        latent_vec = BatchNormalization(axis=-1)(x)
-        encoder = Model(input_layer_encoder, latent_vec)
-
-        for layer in encoder.layers:
-            print(layer.output_shape)
+        self.z_mean = Dense(latent_size)(x)
+        self.z_log_var = Dense(latent_size)(x)
+        self.z = Lambda(self.sampling, output_shape=(latent_size,))([self.z_mean, self.z_log_var])
+        encoder = Model(input_layer_encoder, self.z, name="encoder")
 
         # Creating the decoder
-        input_layer_decoder = Input(shape=encoder.layers[-1].output_shape)
+        input_layer_decoder = Input(shape=(latent_size,))
         x = Dense(512, use_bias=True)(input_layer_decoder)
         x = LeakyReLU(alpha=0.05)(x)
         x = BatchNormalization(axis=-1)(x)
@@ -50,9 +96,29 @@ class AutoEncoder:
                             use_bias=True, padding='same')(x)
         decoder = Model(input_layer_decoder, x, name='decoder')
 
-        autoencoder = Model(input_layer_encoder, decoder(latent_vec), name='autoencoder')
+        ae_output = decoder(encoder(input_layer_encoder))
 
-        for layer in autoencoder.layers:
+        auto_encoder = Model(input_layer_encoder, ae_output, name='autoencoder')
+
+        encoder.summary()
+        decoder.summary()
+        auto_encoder.summary()
+        for layer in auto_encoder.layers:
             print(layer.output_shape)
 
-        return autoencoder, encoder, decoder
+        return auto_encoder, encoder, decoder
+
+    def sampling(self, args):
+        z_mean, z_log_var = args
+        batch = K.shape(z_mean)[0]
+        dim = K.int_shape(z_mean)[1]
+        epsilon = K.random_normal(shape=(batch, dim), dtype=tf.float32)
+        return z_mean + K.exp(0.5 * z_log_var) * epsilon
+
+    def elbo_loss(self, true, pred):
+        # Elbo loss =  binary cross-entropy + KL divergence
+        rec_loss = K.mean(binary_crossentropy(true, pred), axis=[1, 2])
+        #rec_loss = K.mean(true*K.log(pred) + (1-true)*K.log(1-pred), axis=[1])
+        #rec_loss = mse(true, pred)
+        kl_loss = -0.5 * K.mean(1 + self.z_log_var - K.square(self.z_mean) - K.exp(self.z_log_var), axis=[-1])
+        return rec_loss + kl_loss
--- DeepGenerativeModels/main.py
+++ DeepGenerativeModels/main.py
@@ -1,8 +1,11 @@
-from auto_encoder import AutoEncoder
+import auto_encoder as AE
 from verification_net import VerificationNet
 from stacked_mnist import DataMode, StackedMNISTData
 from keras.optimizers import SGD
 import numpy as np
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+
 
 # # # # # Getting the data # # # # #
 # Instantiate the stacked mnist class with a data mode
@@ -45,46 +48,69 @@ import numpy as np
 # - Tensorflow v.1.14.0 w/Keras v.2.2.4 (import keras)
 
 
+def reconstruct_images(auto_encoder, gen, verifier):
+    img, cls = gen.get_full_data_set(training=False)
+    rec_img = auto_encoder.predict(img)
+
+    predictability, accuracy = verifier.check_predictability(rec_img, correct_labels=cls)
+    print(f"Reconstruction predictability is {predictability:.2f} and accuracy is {accuracy:.2f}.")
+
+    img_b, cls_b = gen.get_random_batch(training=False, batch_size=9)
+    gen.plot_example(images=img_b, labels=cls_b)
+    rec_img_b = auto_encoder.predict(img_b)
+    gen.plot_example(images=rec_img_b, labels=cls_b)
+    return rec_img
+
+
+def generate_images(decoder, gen, verification):
+    latent_shape = decoder.layers[0].input_shape[-1]
+    rand_latent_vec = np.random.normal(0, 1, 1000 * latent_shape).reshape(1000, latent_shape)
+    gen_img = decoder.predict(rand_latent_vec)
+    _, rand_cls = gen.get_random_batch(batch_size=9)
+
+    predictability, _ = verification.check_predictability(gen_img)
+    print(predictability)
+    coverage = verification.check_class_coverage(gen_img)
+    print(coverage)
+
+    gen.plot_example(images=gen_img[:9, :, :], labels=rand_cls)
+
+
+def anomaly_detection(auto_encoder, gen, rec_loss_func=lambda x, y: np.mean((x - y) ** 2, axis=(1, 2, 3))):
+    x, cls = gen.get_full_data_set(training=False)
+    rec_img = auto_encoder.predict(x)
+    rec_loss = rec_loss_func(rec_img, x)
+    avg_rec_loss = rec_loss.mean()
+    print(f"Reconstruction loss: {avg_rec_loss:.3f}")
+
+    ind = rec_loss.argsort()[:16]
+    gen.plot_example(images=x[ind], labels=cls[ind])
+    gen.plot_example(images=rec_img[ind], labels=cls[ind])
+
 if __name__ == '__main__':
-    import tensorflow as tf
-    from keras.backend.tensorflow_backend import set_session
+    # TODO: load pivotal parameters
+    # TODO: VAE
+    # TODO: DCGAN
+
+    # Set max gpu usage.
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.7)
     sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
     set_session(sess)
 
-    dataset = "color_binary_complete"
+    dataset = "mono_float_missing"
 
-    gen = StackedMNISTData(mode=DataMode.COLOR_FLOAT_COMPLETE, default_batch_size=9)
-    img, cls = gen.get_random_batch(batch_size=9)
-    #gen.plot_example(img, cls)
+    # Initialize data generator, auto encoder and the verification net.
+    gen = StackedMNISTData(mode=DataMode.MONO_FLOAT_MISSING, default_batch_size=9)
+    x, y = gen.get_random_batch(training=False, batch_size=20000)
+    verifier = VerificationNet(file_name='./models/' + dataset + '.h5')
+    vae = AE.VAE((28, 28, x.shape[-1]), 20)
+    #auto_encoder.load_weights(dataset + '.h5')
+    #auto_encoder.compile(optimizer=SGD(0.01, momentum=0.99), loss=AE.elbo_loss)
+    vae.vae.compile(optimizer=SGD(lr=0.0001, momentum=0.99), loss=vae.elbo_loss)
+    #print(vae.vae.predict(x))
+    vae.vae.fit(x, x, epochs=3, batch_size=8)
 
-    x, y = gen.get_full_data_set(training=True)
-    
-    auto_encoder = AutoEncoder((28, 28, 3), 250)
-    auto_encoder.auto_encoder.compile(optimizer=SGD(0.01, momentum=0.99), loss="binary_crossentropy")
-    auto_encoder.auto_encoder.fit(x, x, batch_size=8, epochs=20)
-    auto_encoder.auto_encoder.save(dataset + '.h5')
 
-    #img = np.around(auto_encoder.auto_encoder.predict(img))
-    auto_encoder.auto_encoder.load_weights(dataset + '.h5')
-    img = auto_encoder.auto_encoder.predict(img)
-    #print((np.random.rand(9, 1, 200) - 0.5 ).shape)
-    #img = auto_encoder.decoder.predict((np.random.rand(9, 1, 200) - 0.5 ) * 2 )
-    #gen.plot_example(img, cls)
-
-    verifier = VerificationNet(file_name = './models/' + dataset + '.h5')
-    verifier.train(gen)
-    img, cls = gen.get_random_batch(batch_size=2000)
-    img = auto_encoder.auto_encoder.predict(img)
-    print(verifier.check_predictability(img, cls))
-   # x, y = gen.get_random_batch(training=True, batch_size=50000)
-   # 
-   # auto_encoder = AutoEncoder((28, 28, 1), 200)
-   # auto_encoder.auto_encoder.compile(optimizer=SGD(0.01, momentum=0.9), loss="mse")
-   # auto_encoder.auto_encoder.fit(x, x, batch_size=8, epochs=15)
-
-   # #img = np.around(auto_encoder.auto_encoder.predict(img))
-   # #img = auto_encoder.auto_encoder.predict(img)
-   # #gen.plot_example(img, cls)
-
-   # auto_encoder.auto_encoder.save('mono_float_missing.h5')
+    reconstruct_images(vae.vae, gen, verifier)
+    generate_images(vae.decoder, gen, verifier)
+    anomaly_detection(vae.vae, gen)
